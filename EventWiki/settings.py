d4409627fa7ac95acc102fc6f0148a4f64f258dc
jeena <jeenakalex@gmail.com>
2022-07-19 18:41:53 +0530

    static folder updated

--- EventWiki/settings.py
+++ EventWiki/settings.py
@@ -119,13 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # STATICFILES_DIRS = [BASE_DIR / "EventApp/static",]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
 
 
