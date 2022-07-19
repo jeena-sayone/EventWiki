ee55c6b1f5d95ba28304420edcb6e5b71cb8a39a
jeena <jeenakalex@gmail.com>
2022-07-19 14:54:38 +0530

    Update settings page

--- EventWiki/settings.py
+++ EventWiki/settings.py
@@ -14,8 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -27,7 +26,7 @@ STRIPE_PUBLIC_KEY = 'pk_test_51LMRNeSH0vrOjKwMLvKa4UG4ER3xGdbA3jydTQo4vWsOErU2Cg
 STRIPE_SECRET_KEY = 'sk_test_51LMRNeSH0vrOjKwMfvYQp0H5kc085AtTVGtNF5iWwQL1w4kyDZV25vH2zphNrlLiAvGvlcN29kmuqhCbzbUgozOo00PdLYQpXX'
 STRIPE_WEBHOOK_SECRET = 'whsec_f0ca155536b7dab732c87954f8ea2e2e37e626312e1810d355a0562cb0c3c8df'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -121,19 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# STATICFILES_DIRS = [BASE_DIR / "EventApp/static",]
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR / "EventApp/static"),
-)
+STATICFILES_DIRS = [BASE_DIR / "EventApp/static",]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 
