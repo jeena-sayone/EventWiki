d1ad5714791ad96d3685440c7a2eec6b95cef61f
jeena <jeenakalex@gmail.com>
2022-07-19 19:45:57 +0530

    Settings file chages

--- EventWiki/settings.py
+++ EventWiki/settings.py
@@ -120,12 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     
 MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]
 
