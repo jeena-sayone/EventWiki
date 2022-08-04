e7cf63c227014d99826e9e7d8bc64f70e255419a
jeena-sayone <109676245+jeena-sayone@users.noreply.github.com>
2022-08-04 10:31:11 +0530

    Revert "Review branch"

--- EventApp/urls.py
+++ EventApp/urls.py
@@ -2,12 +2,10 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index,name='index'),
-    # path('login',views.login,name='login'),
-    path('logout', views.logout, name='logout'),
-    path('login', views.LoginClass.as_view(), name='login'),
+    path('',views.index,name='index'),
+    path('login',views.login,name='login'),
+    path('logout',views.logout,name='logout'),
 
-<<<<<<< HEAD
     path('signup',views.signup,name='signup'),
     path('eventsList',views.eventsList,name='eventsList'),
     path('addEvent',views.addEvent,name='addEvent'),
@@ -18,19 +16,6 @@ urlpatterns = [
     path('payment-success/',views.paymentSuccess,name='payment-success'),
     path('payment-cancel/',views.paymentCancel,name='payment-cancel'),
     path('my_webhook_view',views.my_webhook_view,name='my_webhook_view')
-=======
-    path('signup', views.signup, name='signup'),
-    path('events_list', views.events_list, name='events_list'),
-    path('add_event', views.add_event, name='add_event'),
-    path('edit-event/id=<int:key_id>', views.edit_event, name='edit_event'),
-    path('update_event', views.update_event, name='update_event'),
-    path('delete_event', views.delete_event, name='delete_event'),
-    path('loadPaymentMethod', views.loadPaymentMethod, name='loadPaymentMethod'),
-    path('createCheckoutSession', views.createCheckoutSession, name='createCheckoutSession'),
-    path('payment-success/', views.paymentSuccess, name='payment-success'),
-    path('payment-cancel/', views.paymentCancel, name='payment-cancel'),
-    path('my_webhook_view', views.my_webhook_view, name='my_webhook_view')
->>>>>>> d207192cd6fc1fdd077052236cbf6786d7b73303
 
 
 
